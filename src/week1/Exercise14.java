30fd650c0c7d5abe658ac7183b309657a007e1c0
metallickat <sumannamburi@gmail.com>
2015-05-15 23:26:03 -0400

    Exercise 14

--- src/week1/Exercise14.java
+++ src/week1/Exercise14.java
@@ -3,29 +3,24 @@ package week1;
 import java.util.Scanner;
 
 public class Exercise14 {
-	public static void main(String[] args)
-	{
-		Scanner inscan = new Scanner (System.in);
-		
+	public static void main(String[] args) {
+		Scanner inscan = new Scanner(System.in);
+
 		System.out.println("Enter a number:");
 		int int1 = Integer.parseInt(inscan.nextLine());
-		
-		if (int1 > 0)
-		{
+
+		if (int1 > 0) {
 			System.out.println("The number is +ve");
-		}
-		else if(int1 < 0)
-		{
-			
+		} else if (int1 < 0) {
+
 			System.out.println("The number is -ve");
-			
+
 		}
-		
-		else
-		{
+
+		else {
 			System.out.println("Do you know that 0 is neither +ve nor -ve?");
 		}
-		
+
 	}
 
 }
